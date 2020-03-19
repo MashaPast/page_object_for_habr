a335fd31397a0ef443879d4fc856b95512ebb2e2
Masha <48654187+MashaPast@users.noreply.github.com>
2020-03-19 14:47:29 +0200

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -3,6 +3,6 @@ from selenium import webdriver
 
 @pytest.fixture(scope="session")
 def browser():
-    driver = webdriver.Chrome(executable_path="./chromedriver")
+    driver = webdriver.Chrome()
     yield driver
     driver.quit()
